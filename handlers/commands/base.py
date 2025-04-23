4223e413a7a5a9d8945cf100264936916746e855
catgirl-dev <183555309+catgirl-dev@users.noreply.github.com>
2025-04-23 13:46:44 +0300

    fix admin actualization without db

--- handlers/commands/base.py
+++ handlers/commands/base.py
@@ -3,6 +3,8 @@ from aiogram.filters import Command
 from aiogram.types import Message
 
 from texts.base import help_message_text
+from utils.admins_actualization import get_all_admins, add_chat_to_db
+from utils.captcha.captcha_tools import generate_captcha_config
 
 base: Router = Router()
 
@@ -20,3 +22,6 @@ async def help_message(message: Message):
 @base.message(Command('start'))
 async def start_message(message: Message):
     await message.reply(help_message_text, parse_mode='Markdown')
+    generate_captcha_config(message.chat.id)
+    await add_chat_to_db(message.chat.id)
+    await get_all_admins()
